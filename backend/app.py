ee92dc2ce5907ca0db43293e1a65123ede83fca6
jefrey007 <95954356+jefrey007@users.noreply.github.com>
2025-01-17 16:42:12 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
-from flask_cors import CORS  # Importing CORS
 import psycopg2
 import os
 
-# Initialize Flask app and CORS
 app = Flask(__name__)
-CORS(app)  # Enabling CORS for all routes by default
 
 def get_db_connection():
     connection = psycopg2.connect(
-        host=os.getenv('DB_HOST', 'postgres'),  # Update default to 'postgres'
+        host=os.getenv('DB_HOST', 'localhost'),
         database=os.getenv('DB_NAME', 'mydatabase'),
         user=os.getenv('DB_USER', 'user'),
         password=os.getenv('DB_PASSWORD', 'password')
+    )
+    return connection
+
+@app.route('/')
+def hello():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT message FROM greetings LIMIT 1;')
+    greeting = cursor.fetchone()[0]
+    cursor.close()
+    connection.close()
+    return greeting
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
