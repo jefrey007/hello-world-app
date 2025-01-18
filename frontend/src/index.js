0bf3690815bcd68af3235193d74a637636f339c2
jefrey007 <95954356+jefrey007@users.noreply.github.com>
2025-01-18 22:16:31 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Make sure to import from 'react-dom/client'
-import './index.css';  // Optional, if you want to add some styles
-import App from './App';  // Import your main component
-
-// Create a root and render your app inside it
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import App from './App'; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
